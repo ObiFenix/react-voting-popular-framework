14259ca72181d48805a38cb77bd51fcce7d2122d
ObiFenix <miguel.obiang@mavs.uta.edu>
2018-12-27 12:32:45 -0600

    5th Commit: Modified the VotingPoll component or added feature to it that allows for votes increment.

--- src/components/votingPoll.jsx
+++ src/components/votingPoll.jsx
@@ -6,63 +6,59 @@ import { withStyles } from '@material-ui/core/styles';
 
 
 const styles = theme => ({
-  fab: {
-    margin: theme.spacing.unit,
-  },
-  extendedIcon: {
-    marginRight: theme.spacing.unit,
-  },
+           fab: { margin: theme.spacing.unit, },
+  extendedIcon: { marginRight: theme.spacing.unit, },
 });
 
-function VotingPoll(props) {
 
-  const popularFrameworks = [
-    {
-      name: "React",
-      votes: 15,
-    },
-    {
-      name: "Vue",
-      votes: 12,
-    },
-    {
-      name: "Angular",
-      votes: 9,
-    },
-    {
-      name: "Ember",
-      votes: 4,
-    },
-  ];
+class VotingPoll extends React.Component {
 
-  const { classes } = props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      popularFrameworks: [
+        { name: "React",   votes: 15 },
+        { name: "Vue",     votes: 12 },
+        { name: "Angular", votes: 9  },
+        { name: "Ember",   votes: 4  }
+      ]
+    };
+    this.handleVotes = this.handleVotes.bind(this);
+  }
 
-  const frameworkVote = (nVotes) => (nVotes > 9) ?
+  handleVotes(id) {
+    const updatedFrameworks = [...this.state.popularFrameworks];
+    updatedFrameworks[id].votes += 1;
+    this.setState({updatedFrameworks});
+  }
+
+  frameworkVote = (nVotes) => (nVotes > 9) ?
     (<h3 className="item-votes"><span>{nVotes}</span></h3>) :
     (<h3 className="item-vote"><span>{nVotes}</span></h3>);
 
-  return (
-    <div className="wrapper">
-    {
-      popularFrameworks.map((framework, index) =>
-        <div className="fw-grided">
-          {frameworkVote(framework.votes)}
-          <h3 className="item-name">{framework.name}</h3>
-          <h1 className="item-like">
-            <Fab color="white" aria-label="Add" className={classes.fab}>
-              <AddIcon onClick={ e => alert(`You have voted for ${framework.name}`)}/>
-            </Fab>
-          </h1>
-        </div>
-      )
-    }
-    </div>
-  );
+  render() {
+    return (
+      <div className="wrapper">
+      {
+        this.state.popularFrameworks.map((framework, index) =>
+          <div className="fw-grided">
+            {this.frameworkVote(framework.votes)}
+            <h3 className="item-name">{framework.name}</h3>
+            <h1 className="item-like">
+              <Fab color="white" aria-label="Add" className={this.props.fab}>
+                <AddIcon onClick={ () => this.handleVotes(index) } />
+              </Fab>
+            </h1>
+          </div>
+        )
+      }
+      </div>
+    )
+  }
 }
 
 VotingPoll.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-
 
 export default withStyles(styles)(VotingPoll);
